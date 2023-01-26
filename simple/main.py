3d201b2c514e38510ed772c84a7fdb48f677ac22
si74 <singuva@netflix.com>
2023-01-25 17:36:39 -0800

    adding more todos

--- simple/main.py
+++ simple/main.py
@@ -4,6 +4,8 @@ def main():
 
 
 if __name__ == '__main__':
+    # Use setup.py to setup package/module as script -> https://pybit.es/articles/how-to-package-and-deploy-cli-apps/
+       
     # CLI tool - accept command line arguments - get endpoint we are scanning
 
     # iterate through range of allowable ports and determine what is open vs. closed 
